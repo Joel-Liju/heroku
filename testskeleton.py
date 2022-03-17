f78e457401e8750ab3b55f37fe1428d80cd8268a
Joel-Liju <69165450+Joel-Liju@users.noreply.github.com>
2022-03-16 21:43:37 -0400

    test

--- testskeleton.py
+++ testskeleton.py
@@ -1,4 +1,4 @@
 import app
 
 def testingcalling():
-    assert app.testcall()=="hello"
+    assert app.testcall()=="hello"#right now such succeds, but if changed then the docker file doesn't change
